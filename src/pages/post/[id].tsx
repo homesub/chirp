3a3b81d6037838f611d9f87d1ef43ddaf6e6778f
Andrew Smith <andrewmsmith66@gmail.com>
2023-05-14 10:43:00 -0400

    Adds single post page.

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,19 +1,53 @@
-import { type NextPage } from "next";
+import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 
-const SinglePostPage: NextPage = () => {
+import { api } from "~/utils/api";
+import { PageLayout } from "~/components/layout";
+import { PostView } from "~/components/postview";
+import { generateSSGHelper } from "~/server/helpers/ssgHelper";
+
+const SinglePostPage: NextPage<{ id: string }> = ({ id }) => {
+  const { data } = api.posts.getById.useQuery({
+    id,
+  });
+
+  if (!data) return <div>404</div>;
+
   return (
     <>
       <Head>
-        <title>Post</title>
-        <meta name="description" content="Individual post page for Chirp" />
+        <title>{`${data.post.content} - @${data.author.username}`}</title>
+        <meta name="description" content="User profile page for Chirp" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex h-screen justify-center">
-        <div>Single Post Page</div>
-      </main>
+      <PageLayout>
+        <PostView {...data} />
+      </PageLayout>
     </>
   );
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const ssg = generateSSGHelper();
+
+  const id = context.params?.id as string;
+
+  await ssg.posts.getById.prefetch({ id });
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+      id,
+    },
+    revalidate: 1,
+  };
+};
+
+export const getStaticPaths = () => {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
 };
 
 export default SinglePostPage;
